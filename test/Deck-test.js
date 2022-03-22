7d10ee27264990000b70cc01338d5d49451560f3
Zachary Saile <zwsaile@gmail.com>
2022-03-22 16:26:16 -0600

    Create Deck class and testing file

---
+++ test/Deck-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', () => {
+
+  it('should be a function', () => {
+    const turn = new Deck;
+    expect(Deck).to.be.a('function')
+  });
+
+  it('should be able to store Cards upon instantiation of a new Deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.have.lengthOf(3)
+
+  });
+});
