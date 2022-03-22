f5b9cee1953b35facaf8c4eccd2ac19bd264ad99
Zachary Saile <zwsaile@gmail.com>
2022-03-22 16:33:35 -0600

    Add countCards method and subsequent test to Deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -16,7 +16,15 @@ describe('Deck', () => {
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3]);
 
-    expect(deck.cards).to.have.lengthOf(3)
+    expect(deck.cards).to.be.an("array")
+  });
 
+  it('should be able to count how many cards are in the deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.countCards()).to.equal(3)
   });
 });
