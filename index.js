60fbce1510fc79cea8e895e2efd3a83ef22d5e6f
Zachary Saile <zwsaile@gmail.com>
2022-03-25 08:16:29 -0600

    remove console log

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const data = require('./src/data');
 const prototypeQuestions = data.prototypeData;
 const game = new Game();
 
-console.log('Your project is running...');
-
 game.shuffleDeck();
 game.start()
